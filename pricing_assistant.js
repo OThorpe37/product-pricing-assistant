495d2bd644aceb10d9b15077eb879929b7756cb3
Olivia Thorpe <othorpe@usf.edu>
2026-01-28 15:50:40 -0500

    Calculate Pricing & Profit Metrics

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -5,3 +5,9 @@ let basePrice = 5.00
 let discountRate = .15
 let salesTaxRate = .07
 let fixedMonthlyCosts = 15000.00
+
+let discountedPrice = basePrice * (1-discountRate)
+let finalPriceWithTax = discountedPrice * (1+salesTaxRate)
+let profitPerUnit = finalPriceWithTax - costPerUnit
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit)
+let isProfitablePerUnit = profitPerUnit > 0
